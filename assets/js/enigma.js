33eb20b0bce16234ea3bae89671c3b94d46de8b7
DianaN <nekodikey@gmail.com>
2021-11-23 19:55:48 -0600

    comentario

--- assets/js/enigma.js
+++ assets/js/enigma.js
@@ -1,66 +1,26 @@
-function decodeMorse(morseCode) {
-    var ref = { 
-      '.-':     'a',
-      '-...':   'b',
-      '-.-.':   'c',
-      '-..':    'd',
-      '.':      'e',
-      '..-.':   'f',
-      '--.':    'g',
-      '....':   'h',
-      '..':     'i',
-      '.---':   'j',
-      '-.-':    'k',
-      '.-..':   'l',
-      '--':     'm',
-      '-.':     'n',
-      '---':    'o',
-      '.--.':   'p',
-      '--.-':   'q',
-      '.-.':    'r',
-      '...':    's',
-      '-':      't',
-      '..-':    'u',
-      '...-':   'v',
-      '.--':    'w',
-      '-..-':   'x',
-      '-.--':   'y',
-      '--..':   'z',
-      '.----':  '1',
-      '..---':  '2',
-      '...--':  '3',
-      '....-':  '4',
-      '.....':  '5',
-      '-....':  '6',
-      '--...':  '7',
-      '---..':  '8',
-      '----.':  '9',
-      '-----':  '0',
-    };
-  }
-
-  
-  var abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"]
-  var code = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",".----","..---","...--","....-",".....","-....","--...","---..","----.","-----"]
-
-
-  function Encriptar (){
-  var palabra = document.getElementById("TextoNormal").value;
-  palabra_codificada = btoa(palabra);
-  document.getElementById("TextoEncriptado").innerHTML = palabra_codificada;
-  }
-  function Desencriptar () {
-    var palabra = document.getElementById("TextoNormal").value;
-    palabra_decodificada = atob (palabra);
-    document.getElementById("TextoEncriptado").innerHTML = palabra_decodificada;
-  }
-
-  
-
- 
-  
-  
-  
 
 
  
+
+ function Encriptar (){
+   var palabra = document.getElementById("TextoNormal").value;
+   palabra_codificada = btoa(palabra);
+   document.getElementById("TextoEncriptado").innerHTML = palabra_codificada;
+      }
+
+ function Desencriptar () {
+  var palabra = document.getElementById("TextoEncriptado").value;
+   palabra_decodificada = atob (palabra);
+   document.getElementById("TextoNormal").innerHTML = palabra_decodificada;
+  }
+
+
+  function mostrarContrasena(){
+      var tipo = document.getElementById("password");
+      if(tipo.type == "password"){
+          tipo.type = "text";
+      }else{
+          tipo.type = "password";
+      }
+  }
+
